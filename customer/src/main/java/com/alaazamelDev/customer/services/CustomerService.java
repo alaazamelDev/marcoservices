499994ffa65ebc95f77e48f5000bf813f03346ba
Alaa Zamel <alaa.zamel23@gmail.com>
2024-09-27 17:25:47 +0300

    feat: integrate the customer module with postgresql db, and implement register customer endpoint.

--- customer/src/main/java/com/alaazamelDev/customer/services/CustomerService.java
+++ customer/src/main/java/com/alaazamelDev/customer/services/CustomerService.java
@@ -1,11 +1,12 @@
 package com.alaazamelDev.customer.services;
 
 import com.alaazamelDev.customer.models.Customer;
+import com.alaazamelDev.customer.repositories.CustomerRepository;
 import com.alaazamelDev.customer.requests.RegisterCustomerRequest;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void register(RegisterCustomerRequest request) {
         Customer customer = Customer.builder()
@@ -17,5 +18,6 @@ public record CustomerService() {
         // todo: check if the email is valid
         // todo: check if the email is not taken
         // todo: store customer in db
+        customerRepository.save(customer);
     }
 }
---
+++ customer/src/test/java/com/alaazamelDev/customer/services/CustomerServiceTest.java
@@ -0,0 +1,17 @@
+package com.alaazamelDev.customer.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void register() {
+        // TODO: test the functionality of the register method.
+    }
+}
