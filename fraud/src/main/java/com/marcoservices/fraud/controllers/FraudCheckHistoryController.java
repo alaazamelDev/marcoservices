9ca3110d2503430654971137de3ce5973f063629
Alaa Zamel <alaa.zamel23@gmail.com>
2024-09-30 08:31:42 +0300

    feat: write the complete logic for fraudulent customer check (controller & service)

---
+++ fraud/src/main/java/com/marcoservices/fraud/controllers/FraudCheckHistoryController.java
@@ -0,0 +1,30 @@
+package com.marcoservices.fraud.controllers;
+
+import com.marcoservices.fraud.services.FraudCheckHistoryService;
+import com.marcoservices.utilities.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/fraud-check")
+public record FraudCheckHistoryController(FraudCheckHistoryService service) {
+
+
+    @GetMapping(path = "{customerId}")
+    public ResponseEntity<HashMap<String, Object>> isFraudster(@PathVariable UUID customerId) {
+
+        // call the action
+        Boolean isFraudster = service.isFraudulentCustomer(customerId);
+        HashMap<String, Object> response = ApiResponse.success("checked successfully!", isFraudster);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ fraud/src/main/java/com/marcoservices/utilities/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.marcoservices.utilities;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse {
+
+    private String message;
+    private Integer statusCode;
+    private Object data;
+
+    public static HashMap<String, Object> errorResponse(String message, Integer statusCode) {
+        HashMap<String, Object> responseMap = new HashMap<>();
+        responseMap.put("message", message);
+        responseMap.put("statusCode", statusCode == null ? 400 : statusCode);
+        return responseMap;
+    }
+
+    public static HashMap<String, Object> success(String message, Object data) {
+        HashMap<String, Object> responseMap = new HashMap<>();
+        responseMap.put("message", message);
+        responseMap.put("statusCode", 200);
+        responseMap.put("data", data);
+        return responseMap;
+    }
+
+}
